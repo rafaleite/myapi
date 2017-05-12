be97087e1929c4b3d7d724f64c62f3cd5a1ab31d
Rafael Leite <rafael@rleite.com.br>
2017-05-11 23:26:15 -0300

    Authorization

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express'),
     port = process.env.PORT || 3000,
     mongoose = require('mongoose'),
     Task = require('./models/todoModel'),
+    User = require('./models/userModel'),
+    jwt = require('jsonwebtoken'),
     bodyParser = require('body-parser'),
     routes = require('./routes/todoRoutes');
 
@@ -11,6 +13,19 @@ mongoose.connect('mongodb://192.168.1.4/Tododb');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    if(req.headers && req.headers.authorization) {
+        jwt.verify(req.headers.authorization, 'RESTFULAPIS', function(err, decode) {
+            if(err) req.user = undefined;
+            req.user = decode;
+            next();
+        })
+    }else {
+        req.user = undefined;
+        next();
+    }
+});
 
 routes(app);
 
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,14 +1,21 @@
 'use strict'
 
 module.exports = function(app) {
-    var todo = require('../controllers/todoController');
+    var todo = require('../controllers/todoController'),
+        userHandler = require('../controllers/userController') ;
 
     app.route('/tasks')
         .get(todo.list_all_tasks)
-        .post(todo.create_task);
+        .post(userHandler.loginRequired, todo.create_task);
     
     app.route('/tasks/:id')
         .get(todo.read_task)
         .put(todo.update_task)
         .delete(todo.delete_task);
+
+    app.route('/auth/register')
+        .post(userHandler.register);
+    
+    app.route('/auth/sign_in')
+        .post(userHandler.sign_in);
 }
